b2b31fcf222badd815414ef4b8ccfa24655108a5
MojtabaDavi <mojtabadavi14@gmail.com>
2023-07-27 18:11:23 +0330

    feat: automatic publish action on pypi test Version

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+PROJECT_URL = {
+    "Issues": "https://github.com/Mojitaba34/alert-winglet/issues",
+    "Source Code": "https://github.com/Mojitaba34/alert-winglet",
+}
+setup(project_urls=PROJECT_URL)
