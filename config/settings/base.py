7308f333a34d2fde5aa29d0ba31f343c9479ee8b
MojtabaDavi <mojtabadavi14@gmail.com>
2023-07-31 09:54:52 +0330

    fix: set default value for `ALLOWED_HOSTS` in settings

--- config/settings/base.py
+++ config/settings/base.py
@@ -4,7 +4,7 @@ SECRET_KEY = env.str(
     "SECRET_KEY", "django-insecure-^hx4vco_&p3urjr5p9y1evw7^p%caba0+3p64q=a$!2l&=z=oz"
 )
 
-ALLOWED_HOSTS = env.list("ALLOWED_HOSTS")
+ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", "*")
 CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS")
 
 MIDDLEWARE = [
