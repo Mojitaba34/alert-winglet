909a0b5a1089a908b382b7b0a2536117dda9fe99
MojtabaDavi <mojtabadavi14@gmail.com>
2023-07-31 12:24:04 +0330

    feat: swagger urls and configurations

--- config/settings/swagger.py
+++ config/settings/swagger.py
@@ -1,12 +1,14 @@
 SPECTACULAR_SETTINGS = {
     "SWAGGER_UI_DIST": "SIDECAR",  # shorthand to use the sidecar instead
+    "SWAGGER_UI_SETTINGS": {
+        "persistAuthorization": True,
+    },
     "SWAGGER_UI_FAVICON_HREF": "SIDECAR",
     "REDOC_DIST": "SIDECAR",
-    "TITLE": "Project API",
+    "TITLE": "alert_winglet Project API",
     "DESCRIPTION": "This is a document to understand the API URLs and requirements",
     "VERSION": "1.0.0",
     "SERVE_INCLUDE_SCHEMA": False,
     # OTHER SETTINGS
     "COMPONENT_SPLIT_REQUEST": True,
-    # "SCHEMA_PATH_PREFIX": r"/api/v1/",
 }
--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, re_path
 from django.views.static import serve
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
 
 from config import settings
 
@@ -37,5 +42,19 @@ urlpatterns += [
         kwargs={
             "document_root": settings.STATIC_ROOT,
         },
+    ),
+]
+
+urlpatterns += [
+    path(f"api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        f"api/v1/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        f"api/v1/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
     ),
 ]
