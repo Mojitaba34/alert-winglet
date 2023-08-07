cab46a92e284c7ba1f5470cb2050fbec68b7377d
MojtabaDavi <mojtabadavi14@gmail.com>
2023-08-07 22:50:21 +0330

    fix: some related issues about `apps` prefix and publishing way in `publish.yaml` and directory exclude in `MANIFEST`

--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
 from setuptools import setup
 
+PROJECT_URL = {
+    "Issues": "https://github.com/Mojitaba34/alert-winglet/issues",
+    "Source Code": "https://github.com/Mojitaba34/alert-winglet",
+}
 
-setup(setup_cfg=True, py_modules=[], package_dir={"": "apps"})
+setup(
+    setup_cfg=True,
+    py_modules=[],
+    package_dir={"": "apps"},
+    project_urls=PROJECT_URL,
+    packages=["alert_winglet"],
+)
